3128331377dfe2ea6110f4998e35149204280b46
Arthur Cruz <arthur.silva1041@gmail.com>
2022-05-15 12:15:39 -0300

    password encoding with bcrypt

--- src/main/java/com/artcruz/securitywithspring/security/ApplicationSecurityConfig.java
+++ src/main/java/com/artcruz/securitywithspring/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.artcruz.securitywithspring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,12 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
+
+  private final PasswordEncoder passwordEncoder;
+
+  @Autowired
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,7 +39,11 @@ public class ApplicationSecurityConfig {
 
   @Bean
   public UserDetailsService userDetailsService() {
-    UserDetails annaSmithUser = User.builder().username("annasmith").password("12345678").roles("Student").build();
+    UserDetails annaSmithUser = User.builder()
+      .username("annasmith")
+      .password(passwordEncoder.encode("12345678"))
+      .roles("Student")
+      .build();
 
     return new InMemoryUserDetailsManager(annaSmithUser);
   }
