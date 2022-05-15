1d3dc48b00b79cc084eb03228172b5cb68a4b53b
Arthur Cruz <arthur.silva1041@gmail.com>
2022-05-15 12:02:24 -0300

    in memory user details manager

--- src/main/java/com/artcruz/securitywithspring/security/ApplicationSecurityConfig.java
+++ src/main/java/com/artcruz/securitywithspring/security/ApplicationSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +26,13 @@ public class ApplicationSecurityConfig {
         .httpBasic();
 
     return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails annaSmithUser = User.builder().username("annasmith").password("12345678").roles("Student").build();
+
+    return new InMemoryUserDetailsManager(annaSmithUser);
   }
 
 }
